8afa7d4d75c559a635defb44d729177eb8263539
Sarah Ayanwale <sarahayanwale@gmail.com>
2021-05-28 12:14:12 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using BusStationSimulation.Library;
+using System;
+
+namespace BusStationSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please Enter your name");
+            var name = Console.ReadLine();
+            Console.WriteLine("Please Enter your Phone Number");
+            var PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Please Enter your Destination");
+            var destination = Console.ReadLine();
+            Console.WriteLine("Please Enter your Next of Kin");
+            var nextOfKin = Console.ReadLine();
+
+            Passenger passenger = new Passenger(name, PhoneNumber, destination, nextOfKin);
+            Console.WriteLine();
+            passenger.GetTicket();
+            Console.WriteLine();
+            passenger.MethodCaller();
+        }
+    }
+}
